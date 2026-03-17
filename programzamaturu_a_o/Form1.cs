3de9b6ee4cde44c8472303aacebdfa821a37f90b
thundrevv <terzic.andrej@gmail.com>
2026-03-17 13:22:17 +0100

    pokusaj1

--- programzamaturu_a_o/Form1.cs
+++ programzamaturu_a_o/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,21 @@ namespace programzamaturu_a_o
         public class Ucenik
         {
             public string ime, prezime, odelnje, skola, prvi_predmet, drugi_predmet, treci_predmet, jezik;
+            public void unos(string ime, string prezime, string odelnje, string skola, string prvi_predmet, string drugi_predmet, string treci_predmet, string jezik)
+            {
+                this.ime = ime;
+                this.prezime = prezime;
+                this.odelnje = odelnje;
+                this.skola = skola;
+                this.prvi_predmet = prvi_predmet;
+                this.drugi_predmet = drugi_predmet;
+                this.treci_predmet = treci_predmet;
+                this.jezik = jezik;
+            }
+            public string ispis()
+            {
+                return ime + " " + prezime + " " + odelnje + " " + skola;
+            }
         }
         public Form1()
         {
